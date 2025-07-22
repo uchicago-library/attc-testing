177ec6cc06d087536b209524b827df5deaa37a50
Matt Teichman <teichman@uchicago.edu>
2025-07-22 10:31:56 -0500

    wow, I broke everything

--- GNUmakefile
+++ GNUmakefile
@@ -1,10 +1,13 @@
+$FORCE_TESTS=false
+export FORCE_TESTS
+
 .PHONY: all
 all: mbox-experiments main
 
 .PHONY: mbox-experiments-branch
 mbox-experiments:
-	./update-branches.sh $@
+	env FORCE_TESTS=$(FORCE_TESTS) ./update-branches.sh $@
 
 .PHONY: main-branch
 main:
-	./update-branches.sh $@
+	env FORCE_TESTS=$(FORCE_TESTS) ./update-branches.sh $@
--- update-branches.sh
+++ update-branches.sh
@@ -1,25 +1,33 @@
 #!/bin/sh
 
-# git command to check for updates
-THERE_ARE_UPDATES=git remote update > /dev/null 2> /dev/null && git diff --quiet origin/$1
+# set -e
+# set -x
 
 # name of the sandboxed switch we'll be creating
 SWITCH_NAME=$PWD/$1-branch
 
-if !($THERE_ARE_UPDATES) || [ $FORCE_TESTS ]
-then set -e
-     set -x
-     git -C $1-branch fetch --all
-     git -C $1-branch pull origin $1
-     cd $1-branch 
-     opam switch remove $SWITCH_NAME --yes || true
-     opam switch set ocaml-basics
-     eval $(opam env)
-     opam exec -- dune clean
-     rm -rf _opam || true
-     opam switch create . --deps-only --repos dldc=https://dldc.lib.uchicago.edu/opam,default --yes
-     eval $(opam env)
-     opam exec -- dune exec --display=quiet -- attc --backend=ocamlnet < ../input/kaegiw_inbox.mbox > ../output/kaegiw_inbox_converted_ocamlnet.mbox 2> ../output/kaegiw_inbox_errors_ocamlnet.log
-     opam exec -- dune exec --display=quiet -- attc --backend=mrmime   < ../input/kaegiw_inbox.mbox > ../output/kaegiw_inbox_converted_mrmime.mbox   2> ../output/kaegiw_inbox_errors_mrmime.log
-else echo "update-branches: nothing to do"
+function there_are_updates() {
+    if git -C $1-branch remote update > /dev/null 2> /dev/null && git -C $1-branch diff --quiet origin/$1
+    then false
+    else true
+    fi
+}
+
+if there_are_updates
+# || $FORCE_TESTS
+then echo uh-oh
+    # echo $FORCE_TESTS
+    #  git -C $1-branch fetch --all
+    #  git -C $1-branch pull origin $1
+    #  cd $1-branch 
+    #  opam switch remove $SWITCH_NAME --yes || true
+    #  opam switch set ocaml-basics
+    #  eval $(opam env)
+    #  opam exec -- dune clean
+    #  rm -rf _opam || true
+    #  opam switch create . --deps-only --repos dldc=https://dldc.lib.uchicago.edu/opam,default --yes
+    #  eval $(opam env)
+    #  opam exec -- dune exec --display=quiet -- attc --backend=ocamlnet < ../input/kaegiw_inbox.mbox > ../output/kaegiw_inbox_converted_ocamlnet.mbox 2> ../output/kaegiw_inbox_errors_ocamlnet.log
+    #  opam exec -- dune exec --display=quiet -- attc --backend=mrmime   < ../input/kaegiw_inbox.mbox > ../output/kaegiw_inbox_converted_mrmime.mbox   2> ../output/kaegiw_inbox_errors_mrmime.log
+else echo "update-branches: nothing to do."
 fi
