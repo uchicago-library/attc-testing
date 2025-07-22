edd1da8a838f1d770a01b8e265d12e0d9cf30ff2
Matt Teichman <teichman@uchicago.edu>
2025-07-22 11:00:40 -0500

    ok, I think the checking the branch logic is working, possibly

--- GNUmakefile
+++ GNUmakefile
@@ -1,13 +1,13 @@
-$FORCE_TESTS=false
+FORCE_TESTS=false
 export FORCE_TESTS
 
 .PHONY: all
-all: mbox-experiments main
+all: mbox-experiments
 
 .PHONY: mbox-experiments-branch
 mbox-experiments:
-	env FORCE_TESTS=$(FORCE_TESTS) ./update-branches.sh $@
+	./update-branches.sh $@
 
 .PHONY: main-branch
 main:
-	env FORCE_TESTS=$(FORCE_TESTS) ./update-branches.sh $@
+	./update-branches.sh $@
--- update-branches.sh
+++ update-branches.sh
@@ -13,21 +13,19 @@ function there_are_updates() {
     fi
 }
 
-if there_are_updates
-# || $FORCE_TESTS
-then echo uh-oh
-    # echo $FORCE_TESTS
-    #  git -C $1-branch fetch --all
-    #  git -C $1-branch pull origin $1
-    #  cd $1-branch 
-    #  opam switch remove $SWITCH_NAME --yes || true
-    #  opam switch set ocaml-basics
-    #  eval $(opam env)
-    #  opam exec -- dune clean
-    #  rm -rf _opam || true
-    #  opam switch create . --deps-only --repos dldc=https://dldc.lib.uchicago.edu/opam,default --yes
-    #  eval $(opam env)
-    #  opam exec -- dune exec --display=quiet -- attc --backend=ocamlnet < ../input/kaegiw_inbox.mbox > ../output/kaegiw_inbox_converted_ocamlnet.mbox 2> ../output/kaegiw_inbox_errors_ocamlnet.log
-    #  opam exec -- dune exec --display=quiet -- attc --backend=mrmime   < ../input/kaegiw_inbox.mbox > ../output/kaegiw_inbox_converted_mrmime.mbox   2> ../output/kaegiw_inbox_errors_mrmime.log
+if there_are_updates $1
+# !(git -C $1-branch remote update > /dev/null 2> /dev/null && git -C $1-branch diff --quiet origin/$1) || $FORCE_TESTS
+then git -C $1-branch fetch --all
+     git -C $1-branch pull origin $1
+     cd $1-branch 
+     opam switch remove $SWITCH_NAME --yes || true
+     opam switch set ocaml-basics
+     eval $(opam env)
+     opam exec -- dune clean
+     rm -rf _opam || true
+     opam switch create . --deps-only --repos dldc=https://dldc.lib.uchicago.edu/opam,default --yes
+     eval $(opam env)
+     opam exec -- dune exec --display=quiet -- attc --backend=ocamlnet < ../input/kaegiw_inbox.mbox > ../output/kaegiw_inbox_converted_ocamlnet.mbox 2> ../output/kaegiw_inbox_errors_ocamlnet.log
+     opam exec -- dune exec --display=quiet -- attc --backend=mrmime   < ../input/kaegiw_inbox.mbox > ../output/kaegiw_inbox_converted_mrmime.mbox   2> ../output/kaegiw_inbox_errors_mrmime.log
 else echo "update-branches: nothing to do."
 fi
